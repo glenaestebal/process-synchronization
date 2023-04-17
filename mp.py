bb4e572331872420f8094223c48eaf1862d076a0
amndaprz <amanda_perez@dlsu.edu.ph>
2023-04-17 23:54:24 +0800

    Up

--- mp.py
+++ mp.py
@@ -1,5 +1,6 @@
 import threading
 import time
+import queue
 
 
 # initialization of semaphores
@@ -7,109 +8,128 @@ mutex = 1 #idk what this is for :<
 full_rooms = 0
 blue_count = 0 #counts how many threads inside a room
 green_count = 0 #counts how many threads inside a room
+color = ''
+q = []
+
 
 
 
 # mga blue ang nagbibihis
-def blues_nagbibihis(n, b, id):
+def blues_nagbibihis( n, b, id):
     global blue_count
-        
-    if blue_count +1  <= n: # as long as number of green threads inside the dressing is less than n
-        blue_count+= 1
+    global color  
+    global q  
+    if dressing_rooms_lock.acquire(blocking=False) and green_lock.acquire(blocking = False) :
+                green_lock.release()
+                
+        # if blue_count + 1 < n:
+            # if color == 'b' or color == '':
+                blue_count+= 1
+                #print appropriate string
+                if blue_count == 1: #first to enter
+                    print("Blue only")
+                    #dont allow greens
+                    # door_lock.acquire()
+                    blue_lock.acquire()
+                    color = 'b'
+                    print("Blue thread: ", id, " has entered.")
+                else:
+                    print("Blue thread: ", id, " has entered.")
 
-        #print appropriate string
-        if blue_count == 1: #first to enter
-            print("Blue only")
-            #dont allow blues
-            door_lock.acquire()
-            print("Blue thread: ", id, " has entered.")
-        else:
-            print("Blue thread: ", id, " has entered.")
+                # Log how many threads are in the dressing room
+                # dressing_rooms_lock.acquire()
 
-        # Log how many threads are in the dressing room
-        dressing_rooms_lock.acquire()
+                
+                #green threads will wait 2 seconds inside the fitting before exit
+                time.sleep(2)
 
-        
-        #green threads will wait 2 seconds inside the fitting before exit
-        time.sleep(2)
+                #Exit blue lock
+                print("Blue thread: ", id," exits.") 
+                dressing_rooms_lock.release()
+                blue_count -= 1
 
-        #Exit blue lock
-        print("Blue thread: ", id," exits.") 
-        dressing_rooms_lock.release()
-        blue_count -= 1
-
-        #If dressing room is empty
-        if (blue_count == 0): 
-            print("Dressing room is empty. Blue") 
-            door_lock.release()
-        
+                #If dressing room is empty
+                if (blue_count == 0): 
+                    color = ''
+                    print("Dressing room is empty. Blue") 
+                    blue_lock.release()
+            
     else: #dont allow any threads inside, idk what the threads do while waiting
-        print("Dressing room is full rn. BLue") 
+        # print("Dressing room is full rn. BLue") 
+        q.append(['B',id])
 
 # mga green ang nagbibihis
 def greens_nagbibihis(n, g, id):
     global green_count
-        
-    if green_count +1  <= n: # as long as number of green threads inside the dressing is less than n
-        green_count+= 1
+    global color   
+    global q 
+    if dressing_rooms_lock.acquire(blocking=False) and blue_lock.acquire(blocking = False):
 
-        #print appropriate string
-        if green_count == 1: #first to enter
-            print("Green only")
-            #dont allow blues
-            door_lock.acquire()
-            print("Green thread: ", id, " has entered.")
-        else:
-            print("Green thread: ", id, " has entered.")
+                blue_lock.release()
+        # if green_count + 1 < n:
+        #     if color == 'g' or color == '':
+                green_count+= 1
+                # Log how many threads are in the dressing room
+                # dressing_rooms_lock.acquire()
 
-        # Log how many threads are in the dressing room
-        dressing_rooms_lock.acquire()
+                #print appropriate string
+                if green_count == 1: #first to enter
+                    print("Green only")
+                    #dont allow blues
+                    # door_lock.acquire()
+                    green_lock.acquire()
+                    color = 'g'
+                    print("Green thread: ", id, " has entered.")
+                else:
+                    print("Green thread: ", id, " has entered.")
 
-        
-        #green threads will wait 2 seconds inside the fitting before exit
-        time.sleep(2)
 
-        #Exit green lock
-        print("Green thread: ", id," exits.") 
-        dressing_rooms_lock.release()
-        green_count -= 1
+                
+                #green threads will wait 1 seconds inside the fitting before exit
+                time.sleep(1)
 
-        #If dressing room is empty
-        if (green_count == 0):  
-            print("Dressing room is empty. Green") 
-            door_lock.release()
-        
+                #Exit green lock
+                print("Green thread: ", id," exits.") 
+                dressing_rooms_lock.release()
+                green_count -= 1
+
+                #If dressing room is empty
+                if (green_count == 0):  
+                    color = ''
+                    print("Dressing room is empty. Green") 
+                    green_lock.release()
+            
     else: #dont allow any threads inside, idk what the threads do while waiting
-        print("Dressing room is full rn. Green") 
+        # print("Dressing room is full rn. Green") 
+        q.append(['G',id])
 
 
 
 # input--------------------------------------------
 n, b, g = list(map(int,input().strip().split(" ")))
-empty_rooms = n # all rooms are empty at the start
 
 # array of two semaphores daw: door lock and dressing rooms lock
-door_lock = threading.Semaphore(1) # binary semaphore -- for color, makes sure blue cant enter when green is inside
+green_lock = threading.Semaphore(1) # binary semaphore -- for color, makes sure blue cant enter when green is inside
+blue_lock = threading.Semaphore(1) # binary semaphore -- for color, makes sure blue cant enter when green is inside
+
 dressing_rooms_lock = threading.Semaphore(n) # counting semaphore ; tracks how many threads per room and how many have accessed it 
 
-for x in range(g):
-    green_thread = threading.Thread(target=greens_nagbibihis, args=(n, g,x))   
-    green_thread.start()
-# creating the threads
-for x in range(b):
-    blue_thread = threading.Thread(target=blues_nagbibihis, args=(n, b, x))
-    blue_thread.start()
 
-    
+for i in range(int(b)):
+    q.append(['B',i])
+
+for i in range(int(g)):
+    q.append(['G',i])
 
 
-# starting the threads
-# blue_thread.start()
-# green_thread.start()
+while len(q) > 1:
+    if q[0][0] == 'B':
+        thread_1 = threading.Thread(target = blues_nagbibihis, args = (n,b,q[0][1]))
+        thread_1.start()
+        q.pop(0)
+    if q[0][0] == 'G':
+        thread_1 = threading.Thread(target = greens_nagbibihis, args = (n,g,q[0][1]))
+        thread_1.start()
+        q.pop(0)
 
-# stopping the thread after completion of the task 
-# (not sure kung saan siya kailangan in this problem or kung need ba)
-blue_thread.join()
-green_thread.join()
-
-
+ 
